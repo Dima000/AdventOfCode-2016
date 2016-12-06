545258e42196ce1494cdb5a9fb6198d135849efa
Dumitru.Motpan <dumitru.motpan@gmail.com>
2016-12-06 11:36:19 +0200

    Day6

---
+++ src/main/java/day6/Main.java
@@ -0,0 +1,69 @@
+package day6;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import util.Reader;
+
+public class Main {
+
+	public static final Map<Integer, Map<Character, Integer>> result = new TreeMap<>();
+
+	public static void main(final String[] args) {
+		String[] lines = Reader.readLines("input6.txt");
+		initMaps(lines[0].length());
+
+		for (String line : lines) {
+			for (int i = 0; i < line.length(); i++) {
+				Map<Character, Integer> letterCountMap = result.get(i);
+				char letterToProcess = line.charAt(i);
+				Integer currentCount = letterCountMap.get(letterToProcess);
+				Integer nextCount = currentCount == null ? 0 : currentCount + 1;
+				letterCountMap.put(letterToProcess, nextCount);
+			}
+		}
+
+		for(Map<Character, Integer> letterMap : result.values()) {
+			//puzzle 1
+			//Character toPrint = findLetterWithHighestCount(letterMap);
+
+			//puzzle 2
+			Character toPrint = findLetterWithLowestCount(letterMap);
+			System.out.print(toPrint);
+		}
+
+	}
+
+
+	public static void initMaps(final int length) {
+		for(int i=0;i<length;i++) {
+			result.put(i, new HashMap<Character, Integer>());
+		}
+	}
+
+	public static Character findLetterWithHighestCount(final Map<Character, Integer> lettersMap) {
+		Map.Entry<Character, Integer> highest = null;
+
+		for(Map.Entry<Character, Integer> letter : lettersMap.entrySet()) {
+			if(highest == null || highest.getValue() < letter.getValue()) {
+				highest = letter;
+			}
+		}
+
+		return highest.getKey();
+	}
+
+	public static Character findLetterWithLowestCount(final Map<Character, Integer> lettersMap) {
+		Map.Entry<Character, Integer> lowest = null;
+
+		for(Map.Entry<Character, Integer> letter : lettersMap.entrySet()) {
+			if(lowest == null || lowest.getValue() > letter.getValue()) {
+				lowest = letter;
+			}
+		}
+
+		return lowest.getKey();
+	}
+
+}
